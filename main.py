24b3849b9a5a41e33a20dd71f8685251f4e05757
Santiago Hurtado Galvis <santiagohurtadogalvis5686@gmail.com>
2026-07-07 22:03:16 -0500

    Proyecto guardado

---
+++ main.py
@@ -0,0 +1,299 @@
+
+from service.validacion import validar
+
+from tkinter import*
+
+#manejo del tamaño de la imagen correctamente
+from PIL import Image, ImageTk
+
+#cajas de mensajes
+from tkinter import messagebox 
+
+#ventanas secundarias
+import tkinter as tk
+from tkinter import ttk
+
+
+#base de datos
+
+import sqlite3
+
+
+conexion = sqlite3.connect("bodytype.db")
+cursor = conexion.cursor()
+
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS usuarios (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    usuario TEXT UNIQUE NOT NULL,
+    contrasena TEXT NOT NULL
+)
+""")
+
+conexion.commit()
+
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS historial(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    id_usuario INTEGER,
+    fecha DATETIME DEFAULT CURRENT_TIMESTAMP,
+    porcentaje_grasa REAL,
+    masa_grasa REAL,
+    masa_libre REAL,
+    masa_muscular REAL,
+    tmb REAL,
+    tdee REAL,
+    FOREIGN KEY(id_usuario) REFERENCES usuarios(id)
+)
+""")
+
+conexion.commit()
+
+
+
+cuello_usuario = None
+cintura_usuario = None
+lbl_cintura=None
+altura_usuario = None
+peso_usuario = None
+genero_usuario = None
+edad_usuario=None
+dias_entrenamiento_usuario=None
+cadera_usuario = None
+lbl_cadera = None
+usuario_actual = None
+
+
+
+
+
+
+
+def registrar_usuario():
+
+    nombre = nombre_usuario.get()
+    password = crear_contrasena.get()
+
+    if nombre == "" or password == "":
+        messagebox.showerror("Error", "Complete todos los campos")
+        return
+
+    try:
+
+        cursor.execute(
+            "INSERT INTO usuarios(usuario, contrasena) VALUES (?, ?)",
+            (nombre, password)
+        )
+
+        conexion.commit()
+
+        messagebox.showinfo(
+            "Éxito",
+            "Usuario registrado correctamente"
+        )
+
+    except sqlite3.IntegrityError:
+
+        messagebox.showerror(
+            "Error",
+            "Ese usuario ya existe."
+        )
+
+
+
+
+
+
+
+
+
+
+
+
+def registro():
+
+    global nombre_usuario
+    global crear_contrasena
+
+    ventana_registro=tk.Toplevel()
+    ventana_registro.title("Registro")
+    ventana_registro.iconbitmap("icono.ico")
+    ventana_registro.geometry("500x500")
+    ventana_registro.config(padx=0, pady=0)
+    ventana_registro.grab_set()
+
+
+
+    frame_registro=Frame(ventana_registro,  bg="#111111",
+    bd=2,
+    relief="solid",
+    width=400,
+    height=470,
+    padx=0,
+    pady=0
+    )
+    frame_registro.place(relx=0.5, rely=0.5, anchor="center", width=270, height=400)
+    frame_registro.grid_propagate(False)
+    frame_registro.grid_columnconfigure(0, weight=1)
+
+    imagen = Image.open("fondo_login.jpeg")
+    imagen = imagen.resize((600, 600), Image.Resampling.LANCZOS)
+
+    foto_fondo_validar = ImageTk.PhotoImage(imagen)
+
+
+    label_fondo = Label(ventana_registro, image=foto_fondo_validar)
+    label_fondo.image=foto_fondo_validar
+    label_fondo.place(x=0, y=0, relwidth=1, relheight=1)
+    label_fondo.lower()
+    
+
+    titulo0=Label(frame_registro, text="BodyType Trainer", font=("Arial", 20), bg="#111111", fg="#7AAC05")
+    titulo0.grid(column=0, row=0, sticky="ew")
+
+
+    canvas_logo = Canvas(frame_registro, width=70, height=70,
+                     bd=0, highlightthickness=0, bg="#111111")
+
+    imagen_original = Image.open("login_mejor.png")
+    imagen_redimensionada = imagen_original.resize((70, 70))
+    foto_logo = ImageTk.PhotoImage(imagen_redimensionada)
+
+    canvas_logo.create_image(35, 35, image=foto_logo)
+    canvas_logo.image = foto_logo   # ← ESTA LÍNEA ES LA IMPORTANTE
+
+    canvas_logo.grid(column=0, row=1, pady=10)
+
+
+
+
+    trainer=Label(frame_registro, text="Registrate", font=("Arial", 15), bg="#111111", fg="white", anchor="center", justify="center" )
+    trainer.grid(column=0, row=2)
+
+
+    espacio0=Label(frame_registro, text="", font=("Arial", 10), bg="#111111")
+    espacio0.grid(column=0, row=3)
+
+
+    titulo1=Label(frame_registro, text="Digite su nombre", font=("Arial", 10),  bg="#111111", fg="white")
+    titulo1.grid(column=0, row=4)
+    nombre_usuario=Entry(frame_registro, width=20, font=("Arial", 14), bg="#1E1E1E",fg="white", insertbackground="#7AAC05",relief="flat")
+    nombre_usuario.grid(column=0, row=5, padx=5,pady=5)
+
+
+    titulo2=Label(frame_registro, text="Crea una contraseña segura", font=("Arial", 10),  bg="#111111", fg="white")
+    titulo2.grid(column=0, row=6)
+    crear_contrasena=Entry(frame_registro, width=20, font=("Arial", 14), show="*", bg="#1E1E1E",fg="white", insertbackground="#7AAC05",relief="flat",)
+    crear_contrasena.grid(column=0, row=7, padx=5,pady=5)
+
+    espacio1=Label(frame_registro, text="", font=("Arial", 10), bg="#111111")
+    espacio1.grid(column=0, row=8)
+
+
+    boton_aceptar=Button(frame_registro, text="Aceptar", font=("Arial", 11), pady=5, bg="#111111",fg="#7AAC1D",activebackground="#222222",activeforeground="#A5D62A",highlightthickness=1,highlightbackground="#7AAC1D",bd=0 ,command=registrar_usuario)
+    boton_aceptar.grid(column=0, row=9)
+    
+
+    boton_regresar=Button(frame_registro, text="Regresar",font=("Arial", 11), pady=5, bg="#111111",fg="#7AAC1D",activebackground="#222222",activeforeground="#A5D62A",highlightthickness=1,highlightbackground="#7AAC1D",bd=0, command=ventana_registro.destroy)
+    boton_regresar.grid(column=0, row=10)
+
+
+
+
+
+
+
+
+
+
+ventana_login= Tk()
+ventana_login.title("Login")
+ventana_login.iconbitmap("icono.ico")
+ventana_login.geometry("500x500")
+ventana_login.config(padx=0, pady=0)
+ventana_login.grid_columnconfigure(0, weight=1)
+
+imagen = Image.open("fondo_login.jpeg")
+imagen = imagen.resize((500, 500), Image.Resampling.LANCZOS)
+
+foto_fondo = ImageTk.PhotoImage(imagen)
+
+label_fondo = Label(ventana_login, image=foto_fondo)
+label_fondo.place(x=0, y=0, relwidth=1, relheight=1)
+
+frame_login=Frame(
+
+    ventana_login,
+    bg="#111111",
+    bd=2,
+    relief="solid",
+    padx=3,
+    pady=3
+)
+
+frame_login.place(relx=0.5, rely=0.5, anchor="center")
+
+label_fondo = Label(ventana_login, image=foto_fondo)
+label_fondo.place(x=0, y=0, relwidth=1, relheight=1)
+label_fondo.lower()
+
+
+frame_titulo = Frame(frame_login, bg="#111111")
+frame_login.grid_columnconfigure(0, weight=1)
+frame_login.grid_columnconfigure(1, weight=1)
+frame_login.grid_columnconfigure(2, weight=1)
+frame_titulo.grid(row=0, column=0, columnspan=3, pady=(0, 15))
+
+titulo_inicio = Label(frame_titulo, text="Inicio", font=("Arial", 20, "bold"),
+                      bg="#111111", fg="#7AAC05")
+titulo_inicio.pack(side="left")
+
+titulo_de = Label(frame_titulo, text=" de ", font=("Arial", 20, "bold"),
+                  bg="#111111", fg="#FFFFFF")
+titulo_de.pack(side="left")
+
+titulo_sesion = Label(frame_titulo, text="sesión", font=("Arial", 20, "bold"),
+                      bg="#111111", fg="#7AAC05")
+titulo_sesion.pack(side="left")
+
+
+canvas_logo = Canvas(frame_login, width=70, height=70, bd=0, highlightthickness=0,  bg="#111111")
+
+imagen_original = Image.open("login_mejor.png")
+imagen_redimensionada = imagen_original.resize((70, 70))
+foto_logo = ImageTk.PhotoImage(imagen_redimensionada)
+
+canvas_logo.create_image(35, 35, image=foto_logo)
+canvas_logo.grid(column=0, row=2, columnspan=3, pady=10)
+
+
+
+etiqueta1=Label(frame_login,text="Usuario", font=("Arial", 10), bg="#111111",fg="#CFCFCF")
+etiqueta1.grid(column=0, row=3, columnspan=3)
+usuario=Entry(frame_login,width=15, font=("Arial", 14),bg="#1E1E1E",fg="white", insertbackground="#7AAC05",relief="flat")
+usuario.grid(column=0,row=4, columnspan=3, pady=5)
+
+
+
+etiqueta2=Label(frame_login,text="Contraseña", font=("Arial", 10), bg="#111111",fg="#CFCFCF")
+etiqueta2.grid(column=0, row=5, columnspan=3)
+contrasena=Entry(frame_login, width=15, font=("Arial", 14), show="*", bg="#1E1E1E",fg="white", insertbackground="#7AAC05",relief="flat")
+contrasena.grid(column=0, row=6, columnspan=3, pady=5)
+
+
+etiqueta3=Label(frame_login,text="", font=("Arial", 10), bg="#111111")
+etiqueta3.grid(column=0, row=7)
+
+
+boton_enviar=Button(frame_login,text="Enviar", font=("Arial", 11), pady=5, bg="#111111",fg="#7AAC1D",activebackground="#222222",activeforeground="#A5D62A",highlightthickness=1,highlightbackground="#7AAC1D",bd=0,command=lambda:validar(usuario, contrasena))
+boton_enviar.grid(column=0, row=8, columnspan=3, pady=10)
+
+
+
+boton_registrarse=Button(frame_login,text="Registrarse", font=("Arial", 11), bg="#111111",fg="#7AAC1D",activebackground="#222222",activeforeground="#A5D62A",highlightthickness=1,highlightbackground="#7AAC1D",bd=0, command=registro)
+boton_registrarse.grid(column=0, row=9, columnspan=3)
+
+
+
+
+ventana_login.mainloop()
---
+++ service/historial.py
@@ -0,0 +1,205 @@
+
+from tkinter import*
+from tkinter import ttk
+from PIL import Image, ImageTk
+import tkinter as tk
+from tkinter import messagebox
+
+
+import os
+import sqlite3
+
+BASE_DIR = os.path.dirname(__file__)
+RUTA_BD = os.path.join(BASE_DIR, "..", "bd", "bodytype.db")
+
+conexion = sqlite3.connect(RUTA_BD)
+cursor = conexion.cursor()
+
+
+
+
+
+def borrar_historial(tabla, cursor, conexion, usuario_actual):
+
+    respuesta = messagebox.askyesno(
+        "Confirmar",
+        "¿Desea eliminar todo su historial?"
+    )
+
+    if respuesta:
+
+        cursor.execute(
+            "DELETE FROM historial WHERE id_usuario=?",
+            (usuario_actual,)
+        )
+
+        conexion.commit()
+
+        # Vaciar la tabla en pantalla
+        for item in tabla.get_children():
+            tabla.delete(item)
+
+        messagebox.showinfo(
+            "Historial",
+            "El historial fue eliminado correctamente."
+        )
+
+
+
+
+
+
+
+def historial(usuario_actual):
+
+    ventana_historial=tk.Toplevel()
+    ventana_historial.title("Historial")
+    ventana_historial.iconbitmap("icono.ico")
+    ventana_historial.geometry("700x650")
+    ventana_historial.config(padx=0, pady=0)
+    ventana_historial.grab_set()
+    ventana_historial.grid_columnconfigure(0, weight=1)
+    ventana_historial.grid_columnconfigure(1, weight=1)
+
+
+    imagen = Image.open("fondo_historial.png")
+    imagen = imagen.resize((700, 650), Image.Resampling.LANCZOS)
+
+    foto_fondo_validar = ImageTk.PhotoImage(imagen)
+
+
+    label_fondo = Label(ventana_historial, image=foto_fondo_validar)
+    label_fondo.image=foto_fondo_validar
+    label_fondo.place(x=0, y=0, relwidth=1, relheight=1)
+    label_fondo.lower()
+
+
+    frame_historial = tk.Frame(
+    ventana_historial,
+    bg="#111111",
+    bd=0,
+    highlightthickness=0
+    )
+
+    frame_historial.place( relx=0.5,
+    rely=0.50,   # Ajusta este valor si lo quieres un poco más arriba o abajo
+    anchor="center",
+    width=550,
+    height=440)
+
+    titulo = tk.Label(
+    frame_historial,
+    text="Historial de evaluaciones",
+    font=("Arial", 18, "bold"),
+    bg="#111111",
+    fg="white"
+    )
+
+    titulo.place(relx=0.5, y=30, anchor="n")
+
+
+    style = ttk.Style()
+
+    style.theme_use("clam")   # Permite personalizar los colores
+    style.layout("Treeview", [
+    ("Treeview.treearea", {"sticky": "nswe"})
+    ])
+
+    style.configure(
+    "Treeview",
+    background="#111111",
+    foreground="white",
+    fieldbackground="#111111",
+    rowheight=28,
+    borderwidth=0,
+    relief="flat"
+    )
+
+    style.configure(
+    "Treeview.Heading",
+    background="#111111",
+    foreground="#7AAC1D",
+    borderwidth=0,
+    relief="flat"
+    )
+
+    style.map(
+        "Treeview",
+        background=[("selected", "#222222")],  # Color al seleccionar una fila
+        foreground=[("selected", "white")]
+    )
+
+
+
+
+    tabla = ttk.Treeview(
+    frame_historial,
+    columns=("Fecha", "Grasa", "Masa", "Musculo", "TMB", "TDEE"),
+    show="headings",
+    height=12
+    )
+
+    tabla.heading("Fecha", text="Fecha", anchor="center")
+    tabla.heading("Grasa", text="% Grasa")
+    tabla.heading("Masa", text="Masa grasa")
+    tabla.heading("Musculo", text="Músculo")
+    tabla.heading("TMB", text="TMB")
+    tabla.heading("TDEE", text="TDEE")
+
+    tabla.column("Fecha", width=130)
+    tabla.column("Grasa", width=70, anchor="center")
+    tabla.column("Masa", width=90, anchor="center")
+    tabla.column("Musculo", width=90, anchor="center")
+    tabla.column("TMB", width=80, anchor="center")
+    tabla.column("TDEE", width=80, anchor="center")
+
+    for columna in ("Fecha", "Grasa", "Masa", "Musculo", "TMB", "TDEE"):
+        tabla.column(columna, anchor="center")
+
+    tabla.place(
+    relx=0.5,
+    y=80,
+    anchor="n",
+    width=540,
+    height=260
+    )
+
+    cursor.execute("""
+    SELECT
+    fecha,
+    porcentaje_grasa,
+    masa_grasa,
+    masa_muscular,
+    tmb,
+    tdee
+    FROM historial
+    WHERE id_usuario=?
+    ORDER BY fecha DESC
+    """, (usuario_actual,))
+
+    datos = cursor.fetchall()
+
+    for fila in datos:
+        tabla.insert("", "end", values=fila)
+
+    frame_botones = tk.Frame(
+    frame_historial,
+    bg="#111111"
+    )
+
+    Button(
+    frame_botones,
+    text="Borrar historial",bg="#111111",fg="#7AAC1D",font=("Arial", 15),activebackground="#222222",activeforeground="#A5D62A",highlightthickness=1,highlightbackground="#7AAC1D",bd=0,  command=lambda: borrar_historial(tabla,cursor,conexion,usuario_actual)
+    ).pack(side="left", padx=10)
+
+    Button(
+    frame_botones,
+    text="Cerrar",bg="#111111",fg="#7AAC1D",activebackground="#222222",font=("Arial", 15),activeforeground="#A5D62A",highlightthickness=1,highlightbackground="#7AAC1D",bd=0,
+    command=ventana_historial.destroy,
+    ).pack(side="left", padx=10)
+
+    frame_botones.place(
+    relx=0.5,
+    y=380,
+    anchor="n"
+    )
